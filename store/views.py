4761c17f653b9a1259010eb91b23c0c4933fb14a
lubnariyaz <71705544+lubnariyaz@users.noreply.github.com>
2024-03-27 21:37:16 +0530

    Commit 6: Added product_detail.html, context_processors.py and new banner

--- store/views.py
+++ store/views.py
@@ -8,8 +8,8 @@ def store(request, category_slug=None):
     products = None
 
     if category_slug != None:
-        categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        curr_category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=curr_category, is_available=True)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
@@ -21,3 +21,12 @@ def store(request, category_slug=None):
     }
 
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+
+    context = {
+    'single_product': single_product,
+    }
+
+    return render(request, 'store/product_detail.html', context)
