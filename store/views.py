146d3ca63af4e151c58946ea6c67e19dcbb17aea
lubnariyaz <71705544+lubnariyaz@users.noreply.github.com>
2024-03-30 14:57:17 +0530

    Commit 7: Added Cart Functionality(cart app and cart.html)

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+
+from carts.models import CartItem
+from carts.views import _cart_id
+
+
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -24,9 +29,11 @@ def store(request, category_slug=None):
 
 def product_detail(request, category_slug, product_slug):
     single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
 
     context = {
     'single_product': single_product,
+    'in_cart'       : in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
